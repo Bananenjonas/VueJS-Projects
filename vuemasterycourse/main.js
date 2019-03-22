3210cae8fd4b4d2ae9f44143330a372fa9197ad5
Jonas Herrmannsdörfer <jonasherrmannsdoerfer@outlook.com>
2019-03-22 14:22:40 +0100

    list rendering finished

--- vuemasterycourse/main.js
+++ vuemasterycourse/main.js
@@ -4,6 +4,19 @@ let app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
         image: "./vmSocks-green-onWhite.jpg",
-        website: "https://kahlgrund-webdesign.de/"
+        inventory: 0,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: [8, 9, 10, 11]
     }
 })
