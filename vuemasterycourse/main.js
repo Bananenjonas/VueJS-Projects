93d3ef4eb2247b1cb8088b312404a4bb512dd34f
Jonas Herrmannsdörfer <jonasherrmannsdoerfer@outlook.com>
2019-03-22 13:54:08 +0100

    first commit

---
+++ vuemasterycourse/main.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./vmSocks-green-onWhite.jpg",
+        website: "https://kahlgrund-webdesign.de/"
+    }
+})
